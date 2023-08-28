8b0815d11b065a1fffb7f6f3eeaa78c565cca57b
Aleksandr <tsetserev123@gmail.com>
2023-08-28 18:29:17 +0300

    task 1

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,8 +1,9 @@
 "use strict"
 function solveEquation(a, b, c) {
-  let arr = [];
-  
-  return arr;
+  let d = Math.pow(b, 2)-4*a*c
+  if (d < 0) return []
+  if (d == 0) return [-b/(2*a)]
+  return [(-b + Math.sqrt(d) )/(2*a),(-b - Math.sqrt(d) )/(2*a)]
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
