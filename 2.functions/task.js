08caca4590878146f01fbe28dc272b74912e3e4b
Aleksandr <tsetserev123@gmail.com>
2023-08-31 00:29:07 +0300

    getArrayParams

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,5 +1,17 @@
 function getArrayParams(...arr) {
+  let min = arr[0]
+  let max = arr[0]
+  let avg = 0
 
+  for (let i = 0; i < arr.length; i++) {
+    if (min > arr[i]) min = arr[i]
+    if (max < arr[i]) max = arr[i]
+    avg = avg + arr[i] 
+    if (i == arr.length - 1) {
+      avg = avg / arr.length
+      avg = parseFloat(avg.toFixed(2)) 
+    }
+  }
   return { min: min, max: max, avg: avg };
 }
 
