1bc320106c62136dc331bc4afc564bcc364ce32e
Aleksandr <tsetserev123@gmail.com>
2023-09-02 16:00:14 +0300

    DZ 3 часть

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,8 @@
 function compareArrays(arr1, arr2) {
-  
+    return JSON.stringify(arr1) === JSON.stringify(arr2);
 }
 
 function getUsersNamesInAgeRange(users, gender) {
-  
+  const ages = users.filter((user) => user.gender === gender).map(user => user.age)
+  return ages.reduce((acc, agg) => acc + agg / ages.length, 0)
 }
