7bafbd3447b5bfad24bbbb0ec7d17d693b035387
Aleksandr <tsetserev123@gmail.com>
2023-09-03 18:06:35 +0300

    Full DZ

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,32 @@
 function Student(name, gender, age) {
-  
+    this.name = name;
+    this.gender = gender;
+    this.age = age;
+    this.marks = []
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  
+    this.subject = subjectName;
 }
 
-Student.prototype.addMarks = function (...marks) {
-  
+Student.prototype.addMark = function (mark) {
+    if(!this.marks) return
+    this.marks.push(mark)
+}
+
+Student.prototype.addMarks = function (...mark) {
+    if(!this.marks) return
+    this.marks.push(...mark)
+
 }
 
 Student.prototype.getAverage = function () {
-  
+    if (!this.marks) return 0
+    return this.marks.reduce((acc, mark) => acc + mark / this.marks.length, 0)
 }
 
 Student.prototype.exclude = function (reason) {
-  
+    delete this.subject;
+    delete this.marks;
+    this.excluded = reason;
 }
